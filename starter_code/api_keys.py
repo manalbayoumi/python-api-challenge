2db476c84edd14e790881132cef4494b8a432b5b
manal.bayoumi <manalbayoumi46@gmail.com>
2023-10-25 18:40:24 -0500

    Upload api homework

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "ef48a52a9cf77b0011ffd9ba1a2fd39d"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "db308fd0ab4f461f800b61c9b12441a5"
